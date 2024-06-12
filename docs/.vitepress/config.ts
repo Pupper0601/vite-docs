b0b915d85c57c5f91bd3521ff83f4a5b3a4968af
Pupper <pupper.cheng@gmail.com>
2024-06-12 15:39:34 +0800

    样式调整

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -22,6 +22,13 @@ export default defineConfig({
   markdown: {
     lineNumbers: true,
     theme: 'min-dark',
+    container: {
+      tipLabel: '提示',
+      warningLabel: '警告',
+      dangerLabel: '危险',
+      infoLabel: '信息',
+      detailsLabel: '详细信息',
+    },
   },
 
   /* 主题配置 */
